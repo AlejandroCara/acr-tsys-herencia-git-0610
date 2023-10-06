f97a45e6d2a3c9b84ccae151ba048e3266b88851
jancossio <jancossio@gmail.com>
2023-10-06 18:29:08 +0200

    Clase Profesor

--- Ej5/src/Profesor.java
+++ Ej5/src/Profesor.java
@@ -1,4 +1,28 @@
+import Electrodomestico.colors;
 
 public class Profesor extends Persona{
 
+	private enum materias {matematicas, filosofia, fisica};
+	private Integer disponible;
+	
+	public Profesor(String nombre, int edad, char sexo, String materia) {
+		super(nombre, edad, sexo);
+		this.materia = checkMateria(materia);
+		this.disponible = 20;
+	}
+	
+	public boolean estarPresente() {
+		Random random = new Random();
+		return random.nextDouble()<0.2;
+	}
+	
+	private materias checkMateria(String materia) {
+	    String tmpMater = materia.toLowerCase();
+	    for (materias mater : materias.values()) {
+	    	 if (mater.equals(tmpMater)) {
+	    		 return mater;
+	    	 }
+	    }
+	    return materias.matematicas;
+	}
 }
