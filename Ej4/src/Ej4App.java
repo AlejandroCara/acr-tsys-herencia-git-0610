f111a779e27d402e17d89fbed219390117e7f430
12manel123 <manelcastellvicerezuela@gmail.com>
2023-10-06 19:27:32 +0200

    Ej4 hecho

--- Ej4/src/Ej4App.java
+++ Ej4/src/Ej4App.java
@@ -1,10 +1,20 @@
 public class Ej4App {
 
 	public static void main(String[] args) {
-        // Crear objeto Raices con coeficientes
         Raices raices = new Raices(1, -5, 6); //Ecuacion de: x^2 - 5x + 6 = 0
-        // Calcular y mostrar las raices
         raices.calcular();
+        
+        if (raices.tieneRaices()) {
+            System.out.println("La ecuacion tiene al menos una raiz real.");
+        } else {
+            System.out.println("La ecuacion no tiene raices reales.");
+        }
+
+        if (raices.tieneRaiz()) {
+            System.out.println("La ecuacion tiene una unica raiz real.");
+        } else {
+            System.out.println("La ecuacion no tiene una unica raiz real.");
+        }
     }
 
 }
