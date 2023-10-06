07c575cd592e303c47c2159a4eea51af97e8a402
12manel123 <manelcastellvicerezuela@gmail.com>
2023-10-06 18:02:16 +0200

    Clase persona con getters

--- Ej5/src/Persona.java
+++ Ej5/src/Persona.java
@@ -1,4 +1,24 @@
 
 public class Persona {
+    private String nombre;
+    private int edad;
+    private char sexo;
 
+    public Persona(String nombre, int edad, char sexo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
 }
