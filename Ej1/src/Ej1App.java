be74506f1c7cb7335f33b0e7de484e4a2d5aeb63
12manel123 <manelcastellvicerezuela@gmail.com>
2023-10-06 17:58:05 +0200

    test 3 mane

--- Ej1/src/Ej1App.java
+++ Ej1/src/Ej1App.java
@@ -4,7 +4,7 @@ public class Ej1App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Test Ej1 Alejandro
-		//Test Manel
+		//Test Manel 2
 		System.out.println("Hola");
 	}
 
