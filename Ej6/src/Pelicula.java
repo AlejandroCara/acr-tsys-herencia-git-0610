8262dd028ccc1f4c5d130083dd952953daddb4e6
Alejandro <alejandro.carayol@gmail.com>
2023-10-09 17:30:17 +0200

    si

--- Ej6/src/Pelicula.java
+++ Ej6/src/Pelicula.java
@@ -1,4 +1,33 @@
 
 public class Pelicula {
+	
+	private String titulo = "";
+	private int duracion = 0;
+	private int edadMinimaRecomendada = 0;
+	private String director = "";
+	
+	public Pelicula(String titulo, int duracion, int edadMinimaRecomendada, String director) {
+		this.titulo = titulo;
+		this.duracion = duracion;
+		this.edadMinimaRecomendada = edadMinimaRecomendada;
+		this.director = director;
+	}
 
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public int getEdadMinimaRecomendada() {
+		return edadMinimaRecomendada;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+	
+	
 }
