e384ac025fdcf6aa380636ba4d5d42da0faa2762
jancossio <jancossio@gmail.com>
2023-10-09 17:24:52 +0200

    espectador y asiento

--- Ej6/src/Espectador.java
+++ Ej6/src/Espectador.java
@@ -1,4 +1,38 @@
 
 public class Espectador {
 
+	private String nombre;
+	private Integer edad;
+	private Double dinero;
+	
+	public Espectador() {
+		this.nombre = "";
+		this.edad = 0;
+		this.dinero = 0.0;
+	}
+	
+	public Espectador(String nombre, Integer edad, Double dinero) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.dinero = dinero;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Integer getEdad() {
+		return edad;
+	}
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+	public Double getDinero() {
+		return dinero;
+	}
+	public void setDinero(Double dinero) {
+		this.dinero = dinero;
+	}
 }
