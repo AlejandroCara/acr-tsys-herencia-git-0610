2b9507b16d1d520c432a4825d1e9017af4e594db
12manel123 <manelcastellvicerezuela@gmail.com>
2023-10-09 18:03:30 +0200

    main fix accents

--- Ej6/src/Ej6App.java
+++ Ej6/src/Ej6App.java
@@ -2,26 +2,21 @@
 public class Ej6App {
 
 	public static void main(String[] args) {
-        // Crear una pel�cula
         Pelicula pelicula = new Pelicula("Titanic", 180, 12, "James Cameron");
         
-        // Crear un cine con la pel�cula y precio de entrada
         Cine cine = new Cine(pelicula, 10.0);
         
         Asiento[][] asientos = cine.getAsientos();
 
-        // Crear espectadores
         Espectador espectador1 = new Espectador("Alice", 14, 15.0);
         Espectador espectador2 = new Espectador("Bob", 18, 8.0);
         Espectador espectador3 = new Espectador("Charlie", 22, 12.0);
 
-        // Sentar espectadores en el cine
         cine.sentarEspectador(espectador1);
         cine.sentarEspectador(espectador2);
         cine.sentarEspectador(espectador3);
 
-        // Verificar el estado de los asientos despu�s de sentar a los espectadores
-        System.out.println("Estado de los asientos despu�s de sentar a los espectadores: \n");
+        System.out.println("Estado de los asientos despues de sentar a los espectadores: \n");
         for (int i = 0; i < asientos.length; i++) {
             for (int j = 0; j < asientos[i].length; j++) {
                 System.out.print(asientos[i][j].isOcupado() ? "X " : "O ");
