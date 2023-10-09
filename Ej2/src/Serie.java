d7fccc0f2ed327d7aaa133e2e3aae4331026c388
12manel123 <manelcastellvicerezuela@gmail.com>
2023-10-09 16:32:08 +0200

    a medias clase serie

--- Ej2/src/Serie.java
+++ Ej2/src/Serie.java
@@ -1,4 +1,78 @@
 
 public class Serie implements Entregable{
+	private String titulo;
+    private int numeroTemporadas;
+    private boolean entregado;
+    private String genero;
+    private String creador;
+    
+    public Serie() {
+        this.numeroTemporadas = 3;
+        this.entregado = false;
+    }
+
+    public Serie(String titulo, String creador) {
+        this.titulo = titulo;
+        this.creador = creador;
+        this.numeroTemporadas = 3;
+        this.entregado = false;
+    }
+
+    public Serie(String titulo, int numeroTemporadas, String genero, String creador) {
+        this.titulo = titulo;
+        this.numeroTemporadas = numeroTemporadas;
+        this.genero = genero;
+        this.creador = creador;
+        this.entregado = false;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getNumeroTemporadas() {
+        return numeroTemporadas;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getCreador() {
+        return creador;
+    }
+    @Override
+    public int compareTo(Object objeto) {
+        return Integer.compare(this.numeroTemporadas, objeto.numeroTemporadas);
+    }
+    
+    public String toString() {
+        return "Serie{" +
+                "titulo='" + titulo + '\'' +
+                ", numeroTemporadas=" + numeroTemporadas +
+                ", entregado=" + entregado +
+                ", genero='" + genero + '\'' +
+                ", creador='" + creador + '\'' +
+                '}';
+    }
+
+	@Override
+	public void entregar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void devolver() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEntregado() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
 
 }
